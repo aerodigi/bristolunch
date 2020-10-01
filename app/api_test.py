4bab4d94573948b174e36339571a9dcac70e5d29
Ashley Plane <AeroAppStore@gmail.com>
2020-10-01 09:12:42 +0100

    Updated Commit

--- app/api_test.py
+++ app/api_test.py
@@ -1,23 +1,10 @@
-#calling DATABASE methods
-
-import psycopg2
-import time
 import flask
-import json
+from flask import request, jsonify, render_template, flash
 import sqlite3
-from flask import jsonify, Flask, request, redirect, render_template, flash
-from configparser import ConfigParser
-from config import config
-from psycopg2 import connect
 
-app = Flask(__name__)
+app = flask.Flask(__name__)
+app.config["DEBUG"] = True
 app.secret_key = "6%t6^sl8i3hzozdws*h5fmo#4brcwa**bz01bn0$q57ef7"
-
-conn = psycopg2.connect(
-    host="localhost",
-    dbname="bristolunch_test",
-    user="ash",
-    password="password123")
 
 def dict_factory(cursor, row):
     d = {}
@@ -25,24 +12,11 @@ def dict_factory(cursor, row):
         d[col[0]] = row[idx]
     return d
 
-@app.route("/", methods=["GET","POST"])
-def index_page():
-    if request.method == "POST":
-        result_searchedplace = request.form["searchedplace"]
-        db_result = retrieve_from_db('name')
-        if db_result == None:
-            print("We do not have any information regarding your search on our database.")
-            return "0"
-        else:
-            place_details = dbresult.get["name"]
-            place_details_cuisine = dbresult.get["cuisine"]
-            place_details_address = dbresult.get["address"]
-            place_details_price_range = dbresult.get["price_range"]
-            place_details_webpage = dbresult.get["webpage"]
-            place_details_opening_times = dbresult.get["opening_times"]
-            return render_template("index.html", name=name, cuisine=cuisine, address=address, price_range=price_range, webpage=webpage, opening_times=opening_times)
-    return render_template("index.html")
 
+@app.route('/', methods=['GET'])
+def home():
+    return '''<h1>Distant Reading Archive</h1>
+<p>A prototype API for distant reading of science fiction novels.</p>'''
 
 @app.route("/add_details", methods=["GET", "POST"])
 def add_details_page():
@@ -59,20 +33,6 @@ def add_details_page():
     return render_template("add_details.html")
 
 
-@app.route("/about", methods=["GET"])
-def about_page():
-    return render_template("about.html")
-
-@app.route("/deals", methods=["GET", "POST"])
-def deals_page():
-    return render_template("deals.html")
-
-
-@app.route("/search_results")
-def search_results():
-    pass
-
-
 @app.route('/api/places/all', methods=['GET'])
 def api_all():
     conn = sqlite3.connect('places.db')
@@ -81,7 +41,6 @@ def api_all():
     all_books = cur.execute('SELECT * FROM places;').fetchall()
 
     return jsonify(all_books)
-
 
 
 @app.errorhandler(404)
@@ -130,7 +89,7 @@ def api_filter():
 
     query = query[:-4] + ';'
 
-    conn = sqlite3.connect('places.db')
+    conn = sqlite3.connect('books.db')
     conn.row_factory = dict_factory
     cur = conn.cursor()
 
@@ -138,48 +97,6 @@ def api_filter():
 
     return jsonify(results)
 
-
-def config(filename='database.ini', section='postgresql'):
-    parser = ConfigParser()
-    parser.read(filename)
-
-    db = {}
-    if parser.has_section(section):
-        params = parser.items(section)
-        for param in params:
-            db[param[0]] = param[1]
-    else:
-        raise Exception('Section {0} not found in the {1} file'.format(section, filename))
-
-    return db
-
-def connect():
-
-    conn = None
-    try:
-        param = config()
-        print('Connecting to PostSQL Databased...')
-        conn = psycopg2.connect(
-            dbname ="bristolunch_test",
-            user="ash",
-            host="localhost", 
-            password="password123")
-        cur = conn.cursor()
-        print('PostgreSQL database version:')
-        cur.execute('SELECT version()')
-        db_version = cur.fetchone()
-        print(db_version)
-        cur.close()
-    except (Exception, psycopg2.DatabaseError) as error:
-        print(error)
-    finally:
-        if conn is not None:
-            conn.close()
-            print('Database connection closed.')
-
-
-if __name__ == '__main__':
-    connect()
 
 def insert_into_db(name, cuisine, address, price_range, webpage, opening_times):
     sql = """INSERT INTO places(name, cuisine, address, price_range, webpage, opening_times)
@@ -195,22 +112,45 @@ def insert_into_db(name, cuisine, address, price_range, webpage, opening_times):
     conn.commit()
     print("inserted into Database")
 
+
+app.run()
+
+
+
+# def insert_into_db(name, cuisine, address, price_range, webpage, opening_times):
+#     postgres_insert_query = """INSERT INTO details(name, cuisine, address, price_range, webpage, opening_times)
+#     VALUES(%s, %s, %s, %s, %s, %s) RETURNING id"""
+#     # cursor.execute(postgres_insert_query, 
+#     # (name, cuisine, address, price_range, webpage, opening_times))
+#     # print("inserted into Database")   
+
+#     param = config()
+#     conn = psycopg2.connect(
+#         dbname ="bristolunch_test",
+#         user="ash",
+#         host="localhost", 
+#         password="password123")
+#     cur = conn.cursor()
+#     db_return = cur.execute(postgres_insert_query, (name, cuisine, address, price_range, webpage, opening_times))
+#     conn.commit()
+#     print("inserted into Database")
+
+#     cur.close()
+
+
 def retrieve_from_db(name):
     
-    sql_return_query = """SELECT * FROM places WHERE name = ?"""
+    postgres_return_query = """SELECT * FROM details WHERE name = %s"""
 
     param = config()
-    conn = sqlite3.connect('places.db')
+    conn = psycopg2.connect(
+        dbname ="bristolunch_test",
+        user="ash",
+        host="localhost", 
+        password="password123")
     cur = conn.cursor()
-    cur.execute(sql_return_query, (name,))
+    cur.execute(postgres_return_query, (name,))
     print(cur.fetchone())
     
 
-    results_objects = sjdkljsdjs
-    return json.dumps(results_objects)
-
     cur.close()
-
-
-if __name__ == "__main__":
-    app.run(host="0.0.0.0", debug=True)
